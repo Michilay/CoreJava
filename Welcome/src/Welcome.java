e5dc907d3e89019c7c03df94d4abc7610b6d7fdc
Michilay <1912400157@qq.com>
2021-01-13 15:38:02 +0800

    1.13

--- Welcome/src/Welcome.java
+++ Welcome/src/Welcome.java
@@ -2,7 +2,10 @@
 public class Welcome {
 	public static void main(String[] args)
 	{
-		String greeting = "Welcome to java again!";
+		String greeting = "=Welcome to java again!=";
+		for(int i=0; i<greeting.length();i++)
+			System.out.print("=");
+		System.out.println();
 		System.out.println(greeting);
 		for(int i=0; i<greeting.length();i++)
 			System.out.print("=");
