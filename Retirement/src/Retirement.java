aa59b26090b042f43fe6782dff9c0e1a7efb40ef
Michilay <1912400157@qq.com>
2021-01-18 14:27:16 +0800

    退休金

---
+++ Retirement/src/Retirement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Retirement{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("How much money do you need to retire?");
+		double goal = in.nextDouble();
+		System.out.println("How much money will you contribute every year?");
+		double payment = in.nextDouble();
+		System.out.println("Interest rate in %: ");
+		double interstRate = in.nextDouble();
+		double balance = 0;
+		int years = 0;
+		while(balance < goal)
+		{
+			balance += payment;
+			double interst = balance * interstRate / 100;
+			balance += interst;
+			years++;
+		}
+		System.out.println("You can retire in "+ years + " years.");
+	}
+}
